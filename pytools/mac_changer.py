409949842201991ffc2e8150878dcec20faf779f
jannoh <annohjoseph5@gmail.com>
2024-03-13 19:06:48 +0000

    refactoring & code cleaning

--- pytools/mac_changer.py
+++ pytools/mac_changer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import subprocess
 import optparse
+import re
 
 def change_mac_addr(interface, new_mac_addr):
     print(f"[+] Changing MAC Address for {interface} to {new_mac_addr}")
@@ -19,5 +20,16 @@ def get_arguments():
         parser.error('[-] Please specify an new mac address, use --help for more info.')
     return cli_values
 
+def get_current_mac_addr(interface):
+    ifconfig_result = subprocess.check_output(['ifconfig', interface])
+
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print('[-] Could not read MAC Address')
+
 cli_values = get_arguments()
-change_mac_addr(cli_values.interface, cli_values.new_mac)
+
+current_mac = get_current_mac_addr(cli_values.interface)
+print(f"Current MAC = {current_mac}")
