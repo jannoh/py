d9fc6593ff8acfa7d7da332fc2f205b34d52a8ba
jannoh <annohjoseph5@gmail.com>
2024-03-13 13:26:14 +0000

    introduce variables on mac_changer

--- pytools/mac_changer.py
+++ pytools/mac_changer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.call('ifconfig eth0 down', shell=True)
-subprocess.call('ifconfig eth0 hw ether 00:11:22:33:44:55', shell=True)
-subprocess.call('ifconfig eth0 up', shell=True)
+interface = 'wlan0'
+new_mac_addr = '00:11:22:33:44:77'
+
+print(f"[+] Changing MAC Address for {interface} to {new_mac_addr}")
+
+subprocess.call(f'ifconfig {interface}', shell=True)
+subprocess.call(f'ifconfig {interface} hw ether {new_mac_addr}', shell=True)
+subprocess.call(f'ifconfig {interface} up', shell=True)
