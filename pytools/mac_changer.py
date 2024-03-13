bf41a0598b181d4df956231dfdcaf412cfccdb7d
jannoh <annohjoseph5@gmail.com>
2024-03-13 13:53:48 +0000

    make program secure by using subprocess [] synstax

--- pytools/mac_changer.py
+++ pytools/mac_changer.py
@@ -6,6 +6,6 @@ new_mac_addr = input('new MAC::> ')
 
 print(f"[+] Changing MAC Address for {interface} to {new_mac_addr}")
 
-subprocess.call(f'ifconfig {interface}', shell=True)
-subprocess.call(f'ifconfig {interface} hw ether {new_mac_addr}', shell=True)
-subprocess.call(f'ifconfig {interface} up', shell=True)
+subprocess.call(['ifconfig',interface,'down'])
+subprocess.call(['ifconfig',interface,'hw','ether', new_mac_addr])
+subprocess.call(['ifconfig',interface,'up'])
