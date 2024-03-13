6c53f0e1709f2015f7dc5bbce3bb94e997dee41c
jannoh <annohjoseph5@gmail.com>
2024-03-13 18:01:39 +0000

    make flags required from user

--- pytools/mac_changer.py
+++ pytools/mac_changer.py
@@ -12,8 +12,12 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option('-i', "--interface", dest='interface', help='Interface to change host MAC address')
     parser.add_option('-m', "--mac", dest='new_mac', help='New MAC Address')
-    return parser.parse_args()
+    cli_values, cli_flags = parser.parse_args()
+    if not cli_values.interface:
+        parser.error('[-] Please specify an interface, use --help for more info.')
+    if not cli_values.new_mac:
+        parser.error('[-] Please specify an new mac address, use --help for more info.')
+    return cli_values
 
-cli_values, cli_flags = get_arguments()
+cli_values = get_arguments()
 change_mac_addr(cli_values.interface, cli_values.new_mac)
-
