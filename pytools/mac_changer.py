d106d91eb4a55dbdb6688d1b7d91ac297cb6c092
jannoh <annohjoseph5@gmail.com>
2024-03-13 17:42:31 +0000

    refactored code by using functions

--- pytools/mac_changer.py
+++ pytools/mac_changer.py
@@ -2,17 +2,18 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
-parser.add_option('-i', "--interface", dest='interface', help='Interface to change host MAC address')
-parser.add_option('-m', "--mac", dest='new_mac', help='New MAC Address')
+def change_mac_addr(interface, new_mac_addr):
+    print(f"[+] Changing MAC Address for {interface} to {new_mac_addr}")
+    subprocess.call(['ifconfig',interface,'down'])
+    subprocess.call(['ifconfig',interface,'hw','ether', new_mac_addr])
+    subprocess.call(['ifconfig',interface,'up'])
 
-(cli_values, cli_flags) = parser.parse_args()
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option('-i', "--interface", dest='interface', help='Interface to change host MAC address')
+    parser.add_option('-m', "--mac", dest='new_mac', help='New MAC Address')
+    return parser.parse_args()
 
-interface = cli_values.interface
-new_mac_addr = cli_values.new_mac
+cli_values, cli_flags = get_arguments()
+change_mac_addr(cli_values.interface, cli_values.new_mac)
 
-print(f"[+] Changing MAC Address for {interface} to {new_mac_addr}")
-
-subprocess.call(['ifconfig',interface,'down'])
-subprocess.call(['ifconfig',interface,'hw','ether', new_mac_addr])
-subprocess.call(['ifconfig',interface,'up'])
